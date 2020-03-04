6655d3ea0a3a6229011227437355a944a1915d21
ulkuyasar <35059658+ulkuyasar@users.noreply.github.com>
2020-03-05 00:03:55 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-
+console.log('web-ciftcilik herokuda basliyor');
 // Serve static files....
 app.use(express.static(__dirname + '/dist/web-ciftcilik'));
 
